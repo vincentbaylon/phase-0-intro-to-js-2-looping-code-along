c4ee0a43eb10ec331e27aaef82a74639be722e17
Vincent Baylon <vincentbaylon@gmail.com>
2021-06-15 16:35:04 -0700

    Completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+const gifts = ['teddy bear', 'drone', 'doll'];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(names, eventName) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return messages
+}
+
+function countDown(number) {
+    let i = number;
+    while (number >= 0) {
+        console.log(number--)
+    }    
+}
+
+countDown(10)
